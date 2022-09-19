641d44751493dceddd1fee5e7224f10d43c51eac
Artem <ptv10378@gmail.com>
2022-09-19 22:37:54 +0300

    first commit

---
+++ src/main/java/Post.java
@@ -0,0 +1,9 @@
+public class Post {
+    public FormDate birthday;
+    public String name;
+    public String passport;
+    public String patronymic;
+    public String surname;
+    public String phone;
+    public boolean subscription;
+}
